02fbbfb22c4440d1e885879684381687451591bf
Saad Hassan <saad.haxxan786@gmail.com>
2022-02-11 22:20:43 +0500

    added code for class 1

---
+++ Lectures/01-STL-part1/main.cpp
@@ -0,0 +1,107 @@
+// STL (Standard Template Library)
+
+// Containers
+// Iterators
+// Functors
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+
+using namespace std;
+
+int main()
+{
+	// Vectors
+
+	// vector<int> v={1,2,3,4,5};
+	// cout<<v.size()<<endl;
+
+	// v.push_back(6);
+	// cout<<"Before"<<endl;
+	// for(int i=0;i<v.size();i++){
+	// 	cout<<v[i]<<endl;
+	// }
+	// cout<<"After"<<endl;
+	// v.pop_back();
+	// for(int i=0;i<v.size();i++){
+	// 	cout<<v[i]<<endl;
+	// }
+
+	// cout<<"After Clear"<<endl;
+	// v.clear();
+	// for(int i=0;i<v.size();i++){
+	// 	cout<<v[i]<<endl;
+	// }
+
+	// cout<<v.size()<<endl;
+
+	// Pairs
+
+	// Composite Data type 
+
+	// pair<int, int> p;
+	// p={10,20};
+
+	// cout<<p.first<<endl;
+	// cout<<p.second<<endl;
+
+	// p.insert({20,30})
+
+	// SORT Function
+
+	// int arr[5] = {11,7,15,5,2};
+	// int n = 5;
+	// sort(arr,arr+n);
+
+	// for(int i=0;i<5;i++){
+	// 	cout<<arr[i]<<endl;
+	// }
+
+
+	// vector<int> v={15,23,34,4,5};
+	// sort(v.begin(),v.end(),greater<int>());
+
+	// for(int i=0;i<v.size();i++){
+	// 	cout<<v[i]<<endl;
+	// }
+
+
+	// Iterators
+	// C++ builtin iterator for, while
+
+	// vector<int>::iterator it;
+	
+	// vector<int> v={15,23,34,4,5};
+
+	// for(auto it=v.begin();it!=v.end();it++){
+	// 	cout<<(*it)<<endl;
+	// }
+
+	// auto i=10;
+	
+	// //Ranged Base Loops
+	// for(auto &x:v){
+	// 	cout<<x<<endl;
+	// }
+
+	return 0;
+}
+
+// Arrays
+
+// local array 10^5
+// global array 10^7
+
+
