f12746db10ed005edce34ba764e93bc92d6e1da4
Fawad Ahmad <fawadshah501@gmail.com>
2022-02-18 00:24:06 +0500

    hardcode code done

--- Assignments/Week 01/Find the Kth max and min element of an array.cpp
+++ Assignments/Week 01/Find the Kth max and min element of an array.cpp
@@ -18,6 +18,15 @@ using namespace std;
 int main()
 {
 	//Find the Kth max and min element of an array
-	
+	vector<int> v = {2,3,4,1,10,13,2,45,33};
+	sort(v.begin(), v.end());
+	for(auto i:v){
+		cout<<i<<", ";
+	}
+	cout<<endl;
+	//k = 4
+	int i = v.size(), k = 4;
+	cout<<v[k]<<endl;
+	cout<<v[i-k]<<endl;
 
 }
