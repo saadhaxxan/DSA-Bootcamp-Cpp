2bea675c9ee25f11af14a0f5072bce8f8373a6d1
Muhammad Bilal <47916719+mobilal951@users.noreply.github.com>
2022-02-20 10:54:36 +0500

    Add files via upload
    rearrange array with -ve nums on one side

---
+++ Assignments/rearrange.cpp
@@ -0,0 +1,9 @@
+/*
+Move all negative numbers to beginning and positive to end with constant extra space
+*/
+
+void rearrange(int arr[], int n)
+{
+	sort(arr, arr+n );//just use buil-in sort func and all the -ve nums be moved to one side automatically
+  
+}
