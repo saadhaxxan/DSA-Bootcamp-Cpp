12bc7dd61fc4a7b33725d59bec8340a17907dd94
Muhammad Bilal <47916719+mobilal951@users.noreply.github.com>
2022-02-20 23:08:35 +0500

    Add files via upload

---
+++ Assignments/kth-max and min.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+   
+        sort(arr, arr + r+1);
+    return arr[k - 1];
+    }
+    int kthLargest(int arr[], int l, int r, int k) {
+   
+        sort(arr, arr + r+1);
+    return arr[r-k+1];
+    }
+};
